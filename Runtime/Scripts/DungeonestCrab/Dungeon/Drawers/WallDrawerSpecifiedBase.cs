f709557bf8bd1aebc36f75cdad942fc08d471a95
Kevin Hutchins <possessedcow@gmail.com>
2024-09-08 10:28:41 -0700

    Extract base class from wall drawer specified.

---
+++ Runtime/Scripts/DungeonestCrab/Dungeon/Drawers/WallDrawerSpecifiedBase.cs
@@ -0,0 +1,91 @@
+using Pomerandomian;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonestCrab.Dungeon.Printer {
+    [CreateAssetMenu(menuName = "DungeonestCrab/Drawer/Wall - Specified")]
+    public abstract class WallDrawerSpecifiedBase : IWallDrawer {
+        [SerializeField] Material Material;
+
+        public override void DrawWall(WallInfo info) {
+            // This is the base.
+            GameObject wall = new GameObject($"Wall ({info.tileSpec.Coords.x}, {info.tileSpec.Coords.y})");
+            wall.transform.SetParent(info.parent);
+
+            Quaternion angle = Quaternion.AngleAxis(info.rotation, Vector3.up);
+            Vector3 up = angle * new Vector3(0, info.tileSize.y, 0);
+            Vector3 right = angle * new Vector3(info.tileSize.x, 0, 0);
+            Vector3 back = angle * new Vector3(0, 0, info.tileSize.z);
+
+            GameObject innerWall = new GameObject("DrawnWall");
+            innerWall.transform.SetParent(wall.transform, false);
+            DrawFromConfig(innerWall, info.position, up, right, back, info.minY, info.maxY);
+        }
+
+        protected abstract WallSpec[] GetWallConfiguration();
+
+        void DrawFromConfig(GameObject wall, Vector3 position, Vector3 up, Vector3 right, Vector3 back, float minY, float maxY) {
+            var renderer = wall.AddComponent<MeshRenderer>();
+            renderer.sharedMaterial = Material;
+            var filter = wall.AddComponent<MeshFilter>();
+            var mesh = new Mesh {
+                name = "Wall"
+            };
+            Vector3 basePos = position + -right / 2f + back / 2f;
+            int xVerts = 2;
+
+            WallSpec[] wallSpecs = GetWallConfiguration();
+
+            // Vertices & Mesh
+            var vertices = new Vector3[xVerts * wallSpecs.Length];
+            var uvs = new Vector2[xVerts * wallSpecs.Length];
+            int idx = 0;
+            for (float x = 0; x < xVerts; x++) {
+                for (int y = 0; y < wallSpecs.Length; y++) {
+                    Vector3 vPos = basePos + x * right + wallSpecs[y].percentY * up;
+                    float perturb = wallSpecs[y].outset;
+                    vPos = vPos - back * perturb;
+                    vertices[idx] = vPos;
+                    uvs[idx] = new Vector2(x, wallSpecs[y].uvY);
+                    ++idx;
+                }
+            }
+            mesh.vertices = vertices;
+            mesh.uv = uvs;
+
+            // Triangles
+            var triangles = new int[wallSpecs.Length * xVerts * 2 * 3];
+            idx = -1;
+            for (int x = 0; x < xVerts - 1; x++) {
+                for (int y = 0; y < wallSpecs.Length - 1; y++) {
+                    int baseNum = x * wallSpecs.Length + y;
+                    triangles[++idx] = baseNum;
+                    triangles[++idx] = baseNum + 1;
+                    triangles[++idx] = baseNum + 1 + wallSpecs.Length;
+
+                    triangles[++idx] = baseNum;
+                    triangles[++idx] = baseNum + 1 + wallSpecs.Length;
+                    triangles[++idx] = baseNum + wallSpecs.Length;
+                }
+            }
+            mesh.triangles = triangles;
+            mesh.RecalculateNormals();
+
+            filter.mesh = mesh;
+        }
+
+        [System.Serializable]
+        public class WallSpec {
+            public float percentY = 0;
+            public float outset = 0;
+            public float uvY = 0;
+
+            public WallSpec(float percentY, float outset, float uvY) {
+                this.percentY = percentY;
+                this.outset = outset;
+                this.uvY = uvY;
+            }
+        }
+    }
+}
--- Runtime/Scripts/DungeonestCrab/Dungeon/Drawers/WallDrawerSpecifiedDisjoint.cs
+++ Runtime/Scripts/DungeonestCrab/Dungeon/Drawers/WallDrawerSpecifiedDisjoint.cs
@@ -6,110 +6,56 @@ using UnityEngine;
 
 namespace DungeonestCrab.Dungeon.Printer {
     [CreateAssetMenu(menuName = "DungeonestCrab/Drawer/Wall - Specified Disjoint")]
-    public class WallDrawerSpecifiedDisjoint : IWallDrawer {
-        [SerializeField] WallSpec[] WallConfiguration;
-        [SerializeField] Material Material;
-        private WallSpec[] _cache = null;
+    public class WallDrawerSpecifiedDisjoint : WallDrawerSpecifiedBase {
+        [SerializeField] WallSpecDisjoint[] WallConfiguration;
+        private WallDrawerSpecifiedBase.WallSpec[] _cache = null;
 
         private void OnValidate() {
             _cache = null;
         }
 
-        public override void DrawWall(WallInfo info) {
-            // This is the base.
-            GameObject wall = new GameObject($"Wall ({info.tileSpec.Coords.x}, {info.tileSpec.Coords.y})");
-            wall.transform.SetParent(info.parent);
-
-            Quaternion angle = Quaternion.AngleAxis(info.rotation, Vector3.up);
-            Vector3 up = angle * new Vector3(0, info.tileSize.y, 0);
-            Vector3 right = angle * new Vector3(info.tileSize.x, 0, 0);
-            Vector3 back = angle * new Vector3(0, 0, info.tileSize.z);
-
-            GameObject innerWall = new GameObject("DrawnWall");
-            innerWall.transform.SetParent(wall.transform, false);
-            DrawFromConfig(innerWall, info.position, up, right, back, info.minY, info.maxY);
-        }
-
         WallSpec[] EnsureCache() {
             if (_cache != null) return _cache;
 
-            List<WallSpec> wallSpecs = WallConfiguration.ToList();
+            List<WallSpecDisjoint> wallSpecs = WallConfiguration.ToList();
 
             List<WallSpec> generatedWallSpecs = new List<WallSpec>();
 
             for (int i = 0; i < wallSpecs.Count; i++) {
-                WallSpec curr = wallSpecs[i];
+                WallSpecDisjoint curr = wallSpecs[i];
                 if (curr.endsUVY != curr.startsUVY) {
-                    generatedWallSpecs.Add(new WallSpec(curr.percentY, curr.outset, curr.endsUVY, curr.endsUVY));
+                    generatedWallSpecs.Add(curr.ToWallSpecEnd());
                 }
-                generatedWallSpecs.Add(curr);
+                generatedWallSpecs.Add(curr.ToWallSpecStart());
             }
             _cache = generatedWallSpecs.ToArray();
             return _cache;
         }
 
-        void DrawFromConfig(GameObject wall, Vector3 position, Vector3 up, Vector3 right, Vector3 back, float minY, float maxY) {
-            var renderer = wall.AddComponent<MeshRenderer>();
-            renderer.sharedMaterial = Material;
-            var filter = wall.AddComponent<MeshFilter>();
-            var mesh = new Mesh {
-                name = "Wall"
-            };
-            Vector3 basePos = position + -right / 2f + back / 2f;
-            int xVerts = 2;
-
-            WallSpec[] specs = EnsureCache();
-
-            // Vertices & Mesh
-            var vertices = new Vector3[xVerts * specs.Length];
-            var uvs = new Vector2[xVerts * specs.Length];
-            int idx = 0;
-            for (float x = 0; x < xVerts; x++) {
-                for (int y = 0; y < specs.Length; y++) {
-                    Vector3 vPos = basePos + x * right + specs[y].percentY * up;
-                    float perturb = specs[y].outset;
-                    vPos = vPos - back * perturb;
-                    vertices[idx] = vPos;
-                    uvs[idx] = new Vector2(x, specs[y].startsUVY);
-                    ++idx;
-                }
-            }
-            mesh.vertices = vertices;
-            mesh.uv = uvs;
-
-            // Triangles
-            var triangles = new int[specs.Length * xVerts * 2 * 3];
-            idx = -1;
-            for (int x = 0; x < xVerts - 1; x++) {
-                for (int y = 0; y < specs.Length - 1; y++) {
-                    int baseNum = x * specs.Length + y;
-                    triangles[++idx] = baseNum;
-                    triangles[++idx] = baseNum + 1;
-                    triangles[++idx] = baseNum + 1 + specs.Length;
-
-                    triangles[++idx] = baseNum;
-                    triangles[++idx] = baseNum + 1 + specs.Length;
-                    triangles[++idx] = baseNum + specs.Length;
-                }
-            }
-            mesh.triangles = triangles;
-            mesh.RecalculateNormals();
-
-            filter.mesh = mesh;
+        protected override WallSpec[] GetWallConfiguration() {
+            return EnsureCache();
         }
 
         [System.Serializable]
-        public class WallSpec {
+        public class WallSpecDisjoint {
             public float percentY = 0;
             public float outset = 0;
             public float endsUVY = 0;
             public float startsUVY = 0;
 
-            public WallSpec(float percentY, float outset, float endsUVY, float startsUVY) {
+            public WallSpecDisjoint(float percentY, float outset, float endsUVY, float startsUVY) {
                 this.percentY = percentY;
                 this.outset = outset;
                 this.endsUVY = endsUVY;
                 this.startsUVY = startsUVY;
+            }
+
+            public WallSpec ToWallSpecStart() {
+                return new WallSpec(percentY, outset, startsUVY);
+            }
+
+            public WallSpec ToWallSpecEnd() {
+                return new WallSpec(percentY, outset, endsUVY);
             }
         }
     }
