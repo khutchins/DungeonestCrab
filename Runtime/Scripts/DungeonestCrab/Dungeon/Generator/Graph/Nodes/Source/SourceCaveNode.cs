93c78a469ca45186b440241804b996c60ae472a6
Kevin Hutchins <possessedcow@gmail.com>
2025-12-01 12:35:19 -0800

    Simplify cave node.

--- Runtime/Scripts/DungeonestCrab/Dungeon/Generator/Graph/Nodes/Source/SourceCaveNode.cs
+++ Runtime/Scripts/DungeonestCrab/Dungeon/Generator/Graph/Nodes/Source/SourceCaveNode.cs
@@ -6,10 +6,8 @@ namespace DungeonestCrab.Dungeon.Generator.Graph {
         public Tile TileToSet = Tile.Floor;
         [Range(0, 1)] public float InitialOdds = 0.5f;
         public int MinNeighbors = 3;
-        public int MaxNeighbors = 8;
         public int Iterations = 3;
-        public bool Invert = false;
 
-        public override ISource GetSource() => new SourceCave(TileToSet, InitialOdds, MinNeighbors, MaxNeighbors, Invert, Iterations);
+        public override ISource GetSource() => new SourceCave(TileToSet, InitialOdds, MinNeighbors, 8, false, Iterations);
     }
 }
