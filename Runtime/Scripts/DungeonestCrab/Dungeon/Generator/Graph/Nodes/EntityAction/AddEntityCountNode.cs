af97353c5d0fe961a10968f9d20cd0e6900b2f3e
Kevin Hutchins <possessedcow@gmail.com>
2025-12-01 22:18:22 -0800

    Remove chance from add entity count node. It didn't make sense to be there, as it tries to hit as many as it can (up to the cap).

--- Runtime/Scripts/DungeonestCrab/Dungeon/Generator/Graph/Nodes/EntityAction/AddEntityCountNode.cs
+++ Runtime/Scripts/DungeonestCrab/Dungeon/Generator/Graph/Nodes/EntityAction/AddEntityCountNode.cs
@@ -6,11 +6,10 @@ namespace DungeonestCrab.Dungeon.Generator.Graph {
     public class AddEntityCountNode : EntityPassNode {
         public int MinCount = 1;
         public int TargetCount = 5;
-        [Range(0, 1)] public float Chance = 1f;
 
         protected override bool ApplyNodeLogic(TheDungeon dungeon, IRandom random) {
             EntitySource source = GetEntitySource();
-            return new AddEntity(source, GetMatcher(), AvoidBlockingPath, MinCount, TargetCount, Chance)
+            return new AddEntity(source, GetMatcher(), AvoidBlockingPath, MinCount, TargetCount)
                 .Modify(dungeon, random);
         }
     }
